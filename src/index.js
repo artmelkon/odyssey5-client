ac68adfe432a6b85ee8d1d5468bc63f74d922818
Arthur Melkonyan <arthur@artmelkon.me>
2022-01-08 09:45:24 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  // for local development use => 'http://localhost:4000',
+  uri: "https://odyssey-5.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <GlobalStyles />
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
